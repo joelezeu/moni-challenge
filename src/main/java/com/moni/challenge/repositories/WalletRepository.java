2b9f0c7e56d330a5dfaeeb13d92f65d46d33b4ce
Joel Eze <joelezeu@gmail.com>
2023-10-11 00:20:38 +0100

    Added Pessimistic Lock to the Wallet table

--- src/main/java/com/moni/challenge/repositories/WalletRepository.java
+++ src/main/java/com/moni/challenge/repositories/WalletRepository.java
@@ -3,12 +3,15 @@ package com.moni.challenge.repositories;
 import com.moni.challenge.models.User;
 import com.moni.challenge.models.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Wallet> findByUser(User user);
 }
